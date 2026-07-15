5dc59688be5c886ec7b5b4017e837a65c40e4dc4
Aniket Das <aniketdas162005@gmail.com>
2026-07-15 11:26:40 +0530

    Update settings.py
    Due to unexpected server error 500

--- NeedsMesh/settings.py
+++ NeedsMesh/settings.py
@@ -116,7 +116,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'NeedsMesh <noreply@needsmesh.org>
+DEFAULT_FROM_EMAIL = 'NeedsMesh <noreply@needsmesh.org>'
 
 # For production SMTP:
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
